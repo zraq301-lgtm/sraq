56d6353c0c8ca423727d4bb33aecc01a21fadcee
zraq301-lgtm <zraq301@gmail.com>
2026-02-11 02:20:57 +0200

    Delete android directory

--- android/app/src/main/kotlin/com/example/raqqa/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.example.raqqa
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-    // هذا الكود يربط تطبيقك بمحرك Flutter الحديث (v2) تلقائياً
-}
