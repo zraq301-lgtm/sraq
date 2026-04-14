2a3ec7a3d434d78fc4560ad333fa889e19b5eb6e
zraq301-lgtm <zraq301@gmail.com>
2026-04-14 04:54:35 +0200

    Delete src/pages directory

--- src/pages/Videos.jsx
+++
@@ -1,8 +0,0 @@
-export default function Videos() {
-  return (
-    <div className="page-container">
-      <h1>مكتبة الفيديوهات</h1>
-      <p>استمتعي بمشاهدة محتوى مرئي تعليمي وترفيهي مخصص لكِ.</p>
-    </div>
-  );
-}
